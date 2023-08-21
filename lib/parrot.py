0c3f2efc52e534e8738ab383c2d394711af17e82
Voste126 <steveaustine126@gmail.com>
2023-08-21 14:50:31 +0300

    my work

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,13 @@
-def parrot():
-    pass
+def parrot(speech="Squawk!"):
+    print(speech)
+    return speech
+
+# Adding condition to print "Squawk!" when no argument is passed
+# if __name__ == "__main__":checks if the program is being run as the main program 
+    # speech = input("Enter your speech: ")
+    # if speech:
+    #     parrot(speech)
+    # else:
+    #     parrot()
+
+
